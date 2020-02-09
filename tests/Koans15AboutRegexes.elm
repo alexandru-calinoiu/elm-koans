03114fcbdb27839bee94948c65b09eb1fc325a6f
calin <calinoiu.alexandru@agilefreaks.com>
2020-02-09 17:39:28 +0200

    About regex

--- tests/Koans15AboutRegexes.elm
+++ tests/Koans15AboutRegexes.elm
@@ -12,7 +12,7 @@ Regex.never shall be returned (never matches any input).
 -}
 myRegex : String -> Regex.Regex
 myRegex expression =
-    Regex.fromString expression |> Maybe.withDefault Regex.never
+    Maybe.withDefault Regex.never <| Regex.fromString expression
 
 
 {-| Returns a parsed Regex.Regex for a string with options. If the string cannot
@@ -20,49 +20,49 @@ be parsed, a Regex.never shall be returned (never matches any input).
 -}
 myRegexWith : Regex.Options -> String -> Regex.Regex
 myRegexWith options expression =
-    Regex.fromStringWith options expression |> Maybe.withDefault Regex.never
+    Maybe.withDefault Regex.never <| Regex.fromStringWith options expression
 
 
 testSuite =
     describe "About Regex"
         [ test "contains tests for a regex match" <|
             \() ->
-                x____replace me____x
+                True
                     |> Expect.equal (Regex.contains (myRegex "[abc]+") "abcdefg")
         , test "Regex.never shall never match anything" <|
             \() ->
-                x____replace me____x
+                False
                     |> Expect.equal (Regex.contains Regex.never "anything")
         , test "you need to escape manually special characters" <|
             \() ->
-                x____replace me____x
+                True
                     |> Expect.equal (Regex.contains (myRegex "\\[abc\\]\\+") "hello[abc]+elm")
         , test "it's easy to make your regex case insensitive" <|
             \() ->
-                x____replace me____x
+                True
                     |> Expect.equal (Regex.contains (myRegexWith { caseInsensitive = True, multiline = False } "[abc]+") "ABC")
         , test "find returns a list of all matches" <|
             \() ->
-                x____replace me____x
+                3
                     |> Expect.equal (Regex.find (myRegex "abc") "abcabcabc" |> List.length)
         , test "matches contain the match" <|
             \() ->
-                x____replace me____x
+                [ "a", "b", "c" ]
                     |> Expect.equal (Regex.find (myRegex "[abc]") "axbxc" |> List.map (\match -> match.match))
         , test "matches contain the index" <|
             \() ->
-                x____replace me____x
+                [ 0, 2, 4 ]
                     |> Expect.equal (Regex.find (myRegex "[abc]") "axbxc" |> List.map (\match -> match.index))
         , test "matches may contain submatches" <|
             \() ->
-                x____replace me____x
+                [ [ Just "a", Nothing], [ Nothing, Just "b" ] ]
                     |> Expect.equal (Regex.find (myRegex "(a)|(b)") "axbxc" |> List.map (\match -> match.submatches))
         , test "findAtMost limits the number of matches" <|
             \() ->
-                x____replace me____x
+                [ "a", "b" ]
                     |> Expect.equal (Regex.findAtMost 2 (myRegex "[abc]") "axbxc" |> List.map (\match -> match.match))
         , test "replace can modify based on the match" <|
             \() ->
-                x____replace me____x
+                "AxBxC"
                     |> Expect.equal (Regex.replace (myRegex "[abc]") (\match -> String.toUpper match.match) "axbxc")
         ]
